e1caaf055adc137e32a7ac0ac3b0896e41a8a328
y971 <166815880+yuyan971@users.noreply.github.com>
2025-08-17 10:34:53 +0900

    Add ProjectFile

---
+++ MemoryLens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoryLens
+//
+//  Created by 石松優弥 on 2025/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemoryLens/MemoryLensApp.swift
@@ -0,0 +1,18 @@
+//
+//  MemoryLensApp.swift
+//  MemoryLens
+//
+//  Created by 石松優弥 on 2025/08/17.
+//
+
+import SwiftUI
+
+@main
+struct MemoryLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        
+    }
+}
